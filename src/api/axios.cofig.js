68a5e68c8fb3c2ebcb10ffd6dff368726c2cd505
sj404x <sj404x@us.att.com>
2019-02-26 15:39:38 -0600

    modified basurl axios config to read from public folder

--- src/api/axios.cofig.js
+++ src/api/axios.cofig.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
-const axiosInstance = axios.create({
-  baseURL: "https://api.unsplash.com",
-  headers: {
-    Authorization:
-      "Client-ID 9f236289b764ec3e884027087a199b0ff583d30869d00bb5ea8751956de72a61"
-  }
+export default axios.create({
+    //baseURL: "https://api.unsplash.com",
+    baseURL: process.env.PUBLIC_URL + '/assets/json'
+        /* headers: {
+          Authorization:
+            "Client-ID 9f236289b764ec3e884027087a199b0ff583d30869d00bb5ea8751956de72a61"
+        } */
 });
-
-export default axiosInstance;
